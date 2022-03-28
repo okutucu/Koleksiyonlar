e7e05451e8c1d749fd827197bdee509f3352b069
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2022-03-28 04:15:28 +0300

    Add project files.

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyList<string> isimler = new MyList<string>();
+
+            isimler.Add("Engin");
+        }
+    }
+}
